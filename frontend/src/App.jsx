cc6e91cf3a4337c253ce7dffe498699424767720
unknown <bsitora13@gmail.com>
2026-02-04 17:37:01 +0500

    working on frontend tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>Login route placeholder</div>} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<div>Register route works</div>} />
+        <Route path="/recommend" element={<div>Recommend works</div>} />
       </Routes>
     </Router>
   );

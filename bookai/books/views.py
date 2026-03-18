2c498eeb2d67cba984175308178a0c7029dd4165
unknown <bsitora13@gmail.com>
2026-03-18 17:40:23 +0500

    fixing the bugs

--- bookai/books/views.py
+++ bookai/books/views.py
@@ -9,7 +9,7 @@ from rest_framework import generics, permissions, status, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes, action
-
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 # Local Imports
 from .models import Book, Article, Collection
 from .serializers import ArticleSerializer, CollectionSerializer, BookSerializer
